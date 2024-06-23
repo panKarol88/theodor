b5a8e254e4e82f32c42addd7e8cef5b9354d9ccf
Karol Kamiński <karol.kaminski1990@gmail.com>
2024-06-23 20:57:58 +0200

    trigger redis-server on dev start

--- app/models/data_crumb.rb
+++ app/models/data_crumb.rb
@@ -1,14 +1,17 @@
 class DataCrumb < ApplicationRecord
   validates :content, presence: true
-  after_create_commit -> { broadcast_prepend_to "data_crumbs" }
-  after_update_commit -> { broadcast_replace_to "data_crumbs" }
-  after_destroy_commit -> { broadcast_remove_to "data_crumbs" }
 
-  # equivalent to:
+  # alternative:
   # after_create_commit -> do
-  #   broadcast_prepend_to"data_crumbs",
+  #   broadcast_prepend_to "data_crumbs",
   #                       partial: "data_crumbs/data_crumb",
   #                       locals: { data_crumb: self },
   #                       target: "data_crumbs"
   # end
+
+  # after_create_commit -> { broadcast_prepend_later_to "data_crumbs" }
+  # after_update_commit -> { broadcast_replace_later_to "data_crumbs" }
+  # after_destroy_commit -> { broadcast_remove_to "data_crumbs" }
+
+  broadcasts_to ->(data_crumb) { "data_crumbs" }, inserts_by: :prepend
 end
