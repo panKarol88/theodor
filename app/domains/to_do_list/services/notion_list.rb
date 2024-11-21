f93fc63d692e833bbc6bb5f0bdd8c7507a4f2153
Karol Kamiński <karol.kaminski1990@gmail.com>
2024-11-21 17:07:01 +0100

    notion list improve tasks filter query

--- app/domains/to_do_list/services/notion_list.rb
+++ app/domains/to_do_list/services/notion_list.rb
@@ -20,9 +20,13 @@ module ToDoList
         end
       end
 
+      # ToDoList::Services::NotionList.new(database_id: '143dea0c-ba74-8104-8580-f58a4c054665').tasks(task_filter_params: { is_done: true, days_to_deadline: 7 })
       # :reek:FeatureEnvy
-      def tasks
-        response = notion_request("#{NOTION_API_URL}/databases/#{database_id}/query")
+      def tasks(task_filter_params: {})
+        response = notion_request(
+          "#{NOTION_API_URL}/databases/#{database_id}/query",
+          task_filter_properties(task_filter_params),
+        )
 
         response['results'].map do |item|
           {
@@ -36,6 +40,9 @@ module ToDoList
             task: item.dig('properties', 'Task', 'title', 0, 'plain_text')
           }
         end
+      rescue NoMethodError => error
+        Rails.logger.error(response)
+        raise error
       end
 
       # ToDoList::Services::NotionList.new(database_id: '143dea0c-ba74-8104-8580-f58a4c054665').create_task(task_params: { is_important: false, is_done: false, is_urgent: false, note: nil, deadline: '2024-12-12', task: "eee" })
@@ -52,6 +59,8 @@ module ToDoList
 
       # ToDoList::Services::NotionList.new(database_id: '143dea0c-ba74-8104-8580-f58a4c054665').update_task(page_id: '144dea0c-ba74-8186-9e0a-c4e728d549d2', task_params: { is_important: false, is_done: false, is_urgent: false, note: nil, deadline: '2024-12-12', task: "eee111" })
       def update_task(page_id:, task_params:)
+        return if task_params.blank?
+
         payload = { 'properties': task_properties(task_params) }
 
         notion_request("#{NOTION_API_URL}/pages/#{page_id}", payload, 'patch')
@@ -89,6 +98,28 @@ module ToDoList
           properties['Deadline'] = { 'type': 'date', 'date': { 'start': task_params[:deadline] } } if task_params.key?(:deadline)
           properties['Note'] = { 'type': 'rich_text', 'rich_text': [{ 'type': 'text', 'text': { 'content': task_params[:note] } }] } if task_params.key?(:note)
         end
+      end
+
+      def task_filter_properties(task_search_params)
+        return {} if task_search_params.blank?
+
+        properties = []
+
+        if task_search_params.key?(:is_done)
+          properties << { 'property': 'Done', 'checkbox': { 'equals': task_search_params[:is_done] } }
+        end
+
+        if task_search_params.key?(:days_to_deadline)
+          property = {
+            'property': 'Deadline',
+            'date': {
+              'on_or_before': (Date.today + task_search_params[:days_to_deadline].to_i.days).strftime('%Y-%m-%d')
+            }
+          }
+          properties << property
+        end
+
+        { 'filter': { 'and': properties } }
       end
     end
   end
